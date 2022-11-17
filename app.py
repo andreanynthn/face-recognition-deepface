4d39375a46b22fcb662cb5c0caa6e5fd2e160c77
andreanynthn <and21yonathan@gmail.com>
2022-11-17 20:50:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -179,35 +179,35 @@ def main():
             video_transformer_factory=VideoProcessor
         )
 
-        if ctx.video_transformer:
+        if ctx.state.playing:
             # if st.button("Snapshot"):
             with ctx.video_transformer.frame_lock:
                 image = ctx.video_transformer.img
 
-            if image is not None:
-                img = image#.to_ndarray(format="bgr24")
+                if image is not None:
+                    img = image#.to_ndarray(format="bgr24")
 
-                try:
-                    face_detection = DeepFace.detectFace(img_path = img,
-                                                         target_size = (224, 224),
-                                                         detector_backend = 'ssd'
-                                                         )
-                except:
-                    st.error("Face not detected!")
+                    try:
+                        face_detection = DeepFace.detectFace(img_path = img,
+                                                             target_size = (224, 224),
+                                                             detector_backend = 'ssd'
+                                                             )
+                    except:
+                        st.error("Face not detected!")
 
-                else:
-                    st.success("Face Detected!")
-                    predict, dist = faceRecognition(img)
-
-                    if predict is not None:
-                        if dist <= 0.3:
-                            st.success("Face is successfully recognized.")
-                            st.markdown(f'<h2 style="text-align:center">{string.capwords(predict)}</h2>', unsafe_allow_html=True)
-                            st.image(img)
-                        else:
-                            st.error("Face not recognized.")
                     else:
-                        st.error("Face not registered.")
+                        st.success("Face Detected!")
+                        predict, dist = faceRecognition(img)
+
+                        if predict is not None:
+                            if dist <= 0.3:
+                                st.success("Face is successfully recognized.")
+                                st.markdown(f'<h2 style="text-align:center">{string.capwords(predict)}</h2>', unsafe_allow_html=True)
+                                st.image(img)
+                            else:
+                                st.error("Face not recognized.")
+                        else:
+                            st.error("Face not registered.")
 
 
 
