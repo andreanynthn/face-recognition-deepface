11dd534a987a4e2d7b72eb7430d97a540845becb
andreanynthn <and21yonathan@gmail.com>
2022-11-21 14:15:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import streamlit as st
 from deta import Deta
 from deepface import DeepFace
 from deepface.commons import functions
-from streamlit_webrtc import VideoTransformerBase, webrtc_streamer, ClientSettings, VideoProcessorBase, WebRtcMode
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer, ClientSettings, VideoProcessorBase
 
 
 
@@ -171,46 +171,42 @@ def main():
         if source == "Camera":
             ctx = webrtc_streamer(
                 key="example",
-                # mode=WebRtcMode.SENDONLY,
                 rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
                 media_stream_constraints={"video": True, "audio": False},
                 video_processor_factory=VideoProcessor
             )
 
-            if ctx.video_receiver:
-                # if st.button("Start Recognition"):
-                img = ctx.video_receiver.get_frame(timeout = 1)
-                    # with ctx.video_transformer.frame_lock:
-                    #     image = ctx.video_transformer.img
-                    #
-                    # if image is not None:
-                    #     img = image#.to_ndarray(format="bgr24")
+            if ctx.video_transformer:
+                if st.button("Start Recognition"):
+                    with ctx.video_transformer.frame_lock:
+                        image = ctx.video_transformer.img
 
-                img = img.to_ndarray(format="bgr24")
+                    if image is not None:
+                        img = image#.to_ndarray(format="bgr24")
 
-                try:
-                    face_detection = DeepFace.detectFace(img_path = img,
-                                                         target_size = (224, 224),
-                                                         detector_backend = 'ssd'
-                                                         )
-                except:
-                    st.error("Face not detected!")
+                        try:
+                            face_detection = DeepFace.detectFace(img_path = img,
+                                                                 target_size = (224, 224),
+                                                                 detector_backend = 'ssd'
+                                                                 )
+                        except:
+                            st.error("Face not detected!")
 
-                else:
-                    st.success("Face Detected!")
-                    predict, dist, dur = faceRecognition(img)
-
-                    if predict is not None:
-                        if dist <= 0.3:
-                            st.success("Face is successfully recognized.")
-                            st.markdown(f'<h2 style="text-align:center">{string.capwords(predict)}</h2>', unsafe_allow_html=True)
-                            # st.text(dur)
-
-                            # st.image(img)
                         else:
-                            st.error("Face not recognized.")
-                    else:
-                        st.error("Face not registered.")
+                            st.success("Face Detected!")
+                            predict, dist, dur = faceRecognition(img)
+
+                            if predict is not None:
+                                if dist <= 0.3:
+                                    st.success("Face is successfully recognized.")
+                                    st.markdown(f'<h2 style="text-align:center">{string.capwords(predict)}</h2>', unsafe_allow_html=True)
+                                    # st.text(dur)
+
+                                    # st.image(img)
+                                else:
+                                    st.error("Face not recognized.")
+                            else:
+                                st.error("Face not registered.")
 
         # ----------- file upload --------------
 
